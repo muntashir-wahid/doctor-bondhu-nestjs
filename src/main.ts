30b51a4e514509f7276f372f9262da79ea03a32c
muntashir-wahid <muntashir.wahid@yahoo.com>
2025-10-18 14:41:59 +0600

    initialize the server

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import morgan from 'morgan';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api/v1');
+  app.use(morgan('dev'));
+
+  await app.listen(process.env.PORT ?? 8000);
+}
+bootstrap();
