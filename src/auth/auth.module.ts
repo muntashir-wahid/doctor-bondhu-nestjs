6c8d8602b590f7fd745d4a83e8168d99ae0aaf2a
muntashir-wahid <muntashir.wahid@yahoo.com>
2026-03-14 21:48:14 +0600

    fixed path issue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
-import { HashingModule } from 'src/common/providers/hashing.module';
+import { HashingModule } from '../common/providers/hashing.module';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
