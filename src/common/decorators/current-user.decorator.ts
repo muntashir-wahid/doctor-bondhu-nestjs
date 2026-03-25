5f21b194461d0781dd5e273b7370cd9d689271ab
muntashir-wahid <muntashir.wahid@yahoo.com>
2026-03-25 14:37:26 +0600

    implemented clinic status update

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,13 +1,13 @@
 import { createParamDecorator } from '@nestjs/common';
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { Request } from 'express';
-import { AuthUser } from '../../auth/interfaces/auth-user.interface';
+import { VerifiedAuthUser } from '../../auth/interfaces/auth-user.interface';
 import { REQUEST_USER_KEY } from '../../auth/constants/auth.constants';
 
 export const CurrentUser = createParamDecorator(
-  (data: keyof AuthUser, context: ExecutionContextHost) => {
+  (data: keyof VerifiedAuthUser, context: ExecutionContextHost) => {
     const request = context.switchToHttp().getRequest<Request>();
-    const user = request[REQUEST_USER_KEY] as AuthUser;
+    const user = request[REQUEST_USER_KEY] as VerifiedAuthUser;
 
     if (!user) return null;
 
